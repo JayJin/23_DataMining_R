e3592f7ad1655f023350252d305dfe7ddfa849be
Jay <jvidorca@gmail.com>
2023-03-16 15:59:15 +0900

    Add files for introduction to R

---
+++ 01_R_as_a_calculator.R
@@ -0,0 +1,22 @@
+# R as a Calculator(revisited)
+
+a <- 10
+b <- 2
+sqrt(a)
+
+a + b
+a - b
+a * b
+a / b
+a ^ b
+
+
+exp(b)
+log10(a)
+log2(b)
+a %% (b+1) # modulus after division
+round(1.3456) # round to nearest integer
+floor(1.6456) # round down to nest lowest integer
+ceiling(1.3456) # round up to next largest integer
+choose(a, b) # a combination (choose) b
+sign(a) # sign of a
